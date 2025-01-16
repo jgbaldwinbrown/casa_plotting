b88548a223b850b7e70dec29350baafc7d19f080
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2025-01-16 11:55:34 -0700

    updated extract 3 to look for a trailing c

--- extract_segment_number3.py
+++ extract_segment_number3.py
@@ -27,7 +27,7 @@ def extract_with_regex(instream, outstream, column, ewasteregex, wasteregex, pur
 			eors = segmentmatch.group(2)
 			if not eors:
 				eors = segmentmatch.group(4)
-			outstream.write(stripped_line + " " + eors.lower() + segmentmatch.group(6) + "\n")
+			outstream.write(stripped_line + " " + eors.lower() + segmentmatch.group(6) + segmentmatch.group(7) + "\n")
 			continue
 
 		exit("This is impossible!")
@@ -36,7 +36,7 @@ def main():
 	ewasteregex = re.compile("""[Ee][Ww]aste""")
 	wasteregex = re.compile("""[Ww]aste""")
 	pureregex = re.compile("""[Uu]nloaded|[Pp]ure""")
-	segmentregex = re.compile("""(([Ee])(xtracted)?|([Ss])(egment)?)([1-6])""")
+	segmentregex = re.compile("""(([Ee])(xtracted)?|([Ss])(egment)?)([1-6])([Cc]?)""")
 	extract_with_regex(sys.stdin, sys.stdout, 0, ewasteregex, wasteregex, pureregex, segmentregex)
 
 if __name__ == "__main__":
